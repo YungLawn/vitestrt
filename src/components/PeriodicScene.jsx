ea348d3046d7c22c84a530144fd75ad2ec281a45
YungLawn <ccteelon@gmail.com>
2023-01-31 12:07:32 -0500

    ok

--- src/components/PeriodicScene.jsx
+++ src/components/PeriodicScene.jsx
@@ -62,7 +62,7 @@ export default function PeriodicScene() {
                 </Suspense>          
 
                 <Stars/>
-                <Stats showPanel={4}/>
+                <Stats showPanel={0}/>
             </Canvas>
         </div>
         </>
