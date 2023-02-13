f0b11dc462498867003e892018bd873d2abbe06e
YungLawn <ccteelon@gmail.com>
2023-02-13 17:09:07 -0500

    OK

--- src/components/IsotopeStack.jsx
+++ src/components/IsotopeStack.jsx
@@ -1,22 +1,51 @@
 import {useEffect, useRef, useMemo} from 'react';
 import * as THREE from 'three';
-import { useSpring } from '@react-spring/core'
-import { a } from '@react-spring/three'
+import { animated, useSpring } from '@react-spring/three'
 
 // re-use for instance computations
 const scratchObject3D = new THREE.Object3D();
 const tempColor = new THREE.Color()
 
+function updateInstancedMeshMatrices({ mesh, offsetZ, active }) {
+  if (!mesh) return
+
+  console.log(mesh.getPositionFromMatrix)
+
+  for (let i = 0; i < numPoints; ++i) {
+    // console.log(data[i])
+    const x = 0;
+    const y = 0;
+    const z = (i * 0.3);
+
+    scratchObject3D.position.set(x, y, offsetZ);
+    scratchObject3D.updateMatrix();
+    mesh.setMatrixAt(i, scratchObject3D.matrix);
+    mesh.instanceMatrix.needsUpdate = true
+  }
+}
+
 const IsotopeStack = ({ data, active }) => {
   const meshRef = useRef();
   const numPoints = data.length;
   const colorArray = useMemo(() => Float32Array.from(new Array(data.length).fill().flatMap((_, i) => tempColor.set(data[i]).toArray())), [])
-  const [{ offsetz }] = useSpring({ offsetz: 1, config: { mass: 5, tension: 1000, friction: 50, precision: 0.0001 } }, [active])
-
+  //const [{ offsetZ }] = useSpring({ offsetz: 1, config: { mass: 5, tension: 1000, friction: 50, precision: 0.0001 } }, [active])
+  const { offsetZ } = useSpring({
+    offsetZ: !active ? [0,0,100] : [0,0,0.5],
+    config: { tension: 100, friction: 20, mass: 1, clamp: false, precision: 0.001, velocity: 0.01 }
+  });
 
   // update instance matrices only when needed
   useEffect(() => {
     const mesh = meshRef.current;
+
+    // offsetZ.start({
+    //   from: { offsetZ: hovered ? 1 : 100 },
+    //   to: { offsetZ: hovered ? 100 : 1 },
+    //   reset: true,
+    //   onChange: (props, spring) => {
+    //     updateInstancedMeshMatrices({ mesh: meshRef.current, offsetZ: spring.get(), hovered })
+    //   }
+    // })
 
     // set the transform matrix for each instance
     for (let i = 0; i < numPoints; ++i) {
@@ -26,28 +55,27 @@ const IsotopeStack = ({ data, active }) => {
       const z = (i * 0.3);
 
       scratchObject3D.position.set(x, y, z);
-      // scratchObject3D.rotation.set(0.5 * Math.PI, 0, 0);
       scratchObject3D.updateMatrix();
       mesh.setMatrixAt(i, scratchObject3D.matrix);
     }
 
     mesh.instanceMatrix.needsUpdate = true;
-  }, [numPoints]);
+  });
 
 
     return (
-        <a.instancedMesh
-        position={[0,0,0.5]}
+        <animated.instancedMesh
+        position={offsetZ}
         ref={meshRef}
         args={[null, null, numPoints]}
         // frustumCulled={false}
-        visible={active}
+        // visible={active}
         >
           <boxGeometry attach="geometry" args={[1, 1, 0.25]}>
               <instancedBufferAttribute attach="attributes-color" args={[colorArray, 3]} />
           </boxGeometry>
           <meshStandardMaterial attach="material" vertexColors/>
-        </a.instancedMesh>
+        </animated.instancedMesh>
     );
 
 };
